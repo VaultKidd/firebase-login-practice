110e7936aeb543bf5b8b61be3aba18f176d0d818
Igor <igorkozakiewicz13@gmail.com>
2024-02-17 21:57:39 +0000

    created layout for login menu, need to optimise the visibility of buttons on login and logou, need to add skeleton loading state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css"
+import { auth } from './firebase/init';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Logo from "./assets/Frontend Simplified Logo.png"
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+  const [hideButtons, loggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user)
+      }
+    })
+  }, [])
+
+  function register() {
+    console.log('register')
+    createUserWithEmailAndPassword(auth, 'igor@gmail.com', 'Pass123')
+    .then((user) => {
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  function login() {
+    console.log('login')
+    signInWithEmailAndPassword(auth, 'igor@gmail.com', 'Pass123')
+    .then(({user}) => {
+      console.log(user)
+      if (user) {
+        loggedIn(true)
+      };
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  function logout() {
+    console.log('logout')
+    signOut(auth);
+    setUser({});
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav id="nav">
+        <div className="page__details">
+          <button className="btn menu">
+            <FontAwesomeIcon icon="bars" />
+          </button>
+          <img className="logo" src={Logo} />
+        </div>
+        <div className="login__details">
+          {hideButtons ? null : (<button className="btn login__btn" onClick={login}>Login</button>)}
+          {hideButtons ? null : (<button className="btn register__btn" onClick={() => register}>Register</button>)}
+          {hideButtons ? (<button className="btn logout__btn" onClick={logout}>{user.email}</button>) : null}
+          {/* {loading ? 'Loading...' : user.email} */}
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxn7zojp0qWUAZJ1TB3q3FJtFVdaEjrAw",
+  authDomain: "fir-practice-83066.firebaseapp.com",
+  projectId: "fir-practice-83066",
+  storageBucket: "fir-practice-83066.appspot.com",
+  messagingSenderId: "510765149958",
+  appId: "1:510765149958:web:4765227fdb616cbbcdb887"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
