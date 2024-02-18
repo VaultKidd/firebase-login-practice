08fa370c7486f8ce4d0c6adba4ba59772b447cea
Igor <igorkozakiewicz13@gmail.com>
2024-02-18 15:01:52 +0000

    created a log in menu, which logs user in based on email and password, and then logs them out, also remembers log in on refresh

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React from "react";
-import "./index.css"
-import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Logo from "./assets/Frontend Simplified Logo.png"
+import "./index.css";
+import { auth } from "./firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Logo from "./assets/Frontend Simplified Logo.png";
 
 function App() {
   const [user, setUser] = React.useState({});
-  const [loading, setLoading] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
   const [hideButtons, loggedIn] = React.useState(false);
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setLoading(false);
       console.log(user);
       if (user) {
-        setUser(user)
+        setUser(user);
+        loggedIn(true)
       }
-    })
-  }, [])
+    });
+  }, []);
 
   function register() {
-    console.log('register')
-    createUserWithEmailAndPassword(auth, 'igor@gmail.com', 'Pass123')
-    .then((user) => {
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "igor@gmail.com", "Pass123")
+      .then((user) => {})
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function login() {
-    console.log('login')
-    signInWithEmailAndPassword(auth, 'igor@gmail.com', 'Pass123')
-    .then(({user}) => {
-      console.log(user)
-      if (user) {
-        loggedIn(true)
-      };
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+    console.log("login");
+    signInWithEmailAndPassword(auth, "igor@gmail.com", "Pass123")
+      .then(({ user }) => {
+        console.log(user);
+        if (user) {
+          loggedIn(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function logout() {
-    console.log('logout')
+    console.log("logout");
     signOut(auth);
     setUser({});
-
+    loggedIn(false)
   }
 
   return (
@@ -61,10 +65,22 @@ function App() {
           <img className="logo" src={Logo} />
         </div>
         <div className="login__details">
-          {hideButtons ? null : (<button className="btn login__btn" onClick={login}>Login</button>)}
-          {hideButtons ? null : (<button className="btn register__btn" onClick={() => register}>Register</button>)}
-          {hideButtons ? (<button className="btn logout__btn" onClick={logout}>{user.email}</button>) : null}
-          {/* {loading ? 'Loading...' : user.email} */}
+          {hideButtons ? null : (
+            <button className="btn login__btn" onClick={login}>
+              Login
+            </button>
+          )}
+          {hideButtons ? null : (
+            <button className="btn register__btn" onClick={() => register}>
+              Register
+            </button>
+          )}
+          {hideButtons ? (
+            <button className="btn logout__btn" onClick={logout}>
+              {user.email[0].toUpperCase()}
+            </button>
+          ) : null} 
+          {loading ? 'Loading...' : null}
         </div>
       </nav>
     </div>
